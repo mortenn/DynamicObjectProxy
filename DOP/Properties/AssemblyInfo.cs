931a031ebc326b9f1d31e3c73fced77d5c23cac3
Morten Nilsen <mortenn@hapro.no>
2017-07-25 12:10:25 +0200

    nuget.org refused same name package, so renamed it.

--- DOP/Properties/AssemblyInfo.cs
+++ DOP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynamicProxy")]
+[assembly: AssemblyTitle("DynamicProxyNew")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Latitude Consulting Group")]
-[assembly: AssemblyProduct("DynamicProxy")]
+[assembly: AssemblyProduct("DynamicProxyNew")]
 [assembly: AssemblyCopyright("Copyright © Latitude Consulting Group 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
